aac59a00fa57502733460a5a59c3e09f22a8ce7c
noobtuber20103152 <81584747+noobtuber20103152@users.noreply.github.com>
2022-02-02 16:19:40 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -13,46 +13,59 @@ function App() {
   const [bgColor, SetbgColor] = useState('light')
   const [DarkText, SetDarkText] = useState('Dark');
   const [TextColor, SetTextColor] = useState({
-    color:"black"
+    color: "black"
   });
-  const[dark, Setdark] = useState('light');
-  const[light, Setlight] = useState('dark')
-  const ChangeMode = function(){
-    if(bgColor==='light'){
+  const [dark, Setdark] = useState('light');
+  const [light, Setlight] = useState('dark')
+  const ChangeMode = function () {
+    if (bgColor === 'light') {
       SetbgColor('dark');
       SetDarkText('Light')
       SetTextColor({
-        color:"white"
+        color: "white"
       })
-      document.body.style.backgroundColor= "black"
+      document.body.style.backgroundColor = "black"
       Setdark('dark')
       Setlight('light')
     }
-    else{
+    else {
       SetbgColor('light');
       SetDarkText('Dark')
       SetTextColor({
-        color:"black"
+        color: "black"
       })
-      document.body.style.backgroundColor= "white"
+      document.body.style.backgroundColor = "white"
       Setdark('light')
       Setlight('dark')
     }
-}
+  }
   return (
     <>
-    <Navbar bgColor = {bgColor} TextColor = {TextColor} DarkText={DarkText} ChangeMode={ChangeMode}/>
-    <BrowserRouter>
-          <Routes>
-            <Route exact path={"/"} element={<News dark = {dark} light = {light} TextColor={TextColor} key="general"  category="general" />} />
-            <Route exact path={"/sports"} element={<News dark = {dark} light = {light} TextColor={TextColor} key="sports" category="sports" />} />
-            <Route exact path={"/science"} element={<News dark = {dark} light = {light}  TextColor={TextColor} key="science" category="science" />} />
-            <Route exact path={"/technology"} element={<News dark = {dark} light = {light} TextColor={TextColor} key="technology" category="technology" />} />
-            <Route exact path={"/business"} element={<News dark = {dark} light = {light} TextColor={TextColor} key="technology" category="business" />} />
-            <Route exact path={"/health"} element={<News dark = {dark} light = {light} TextColor={TextColor} key="technology" category="health" />} />
-            <Route exact path={"/entertainment"} element={<News dark = {dark} light = {light} TextColor={TextColor} key="technology" category="entertainment" />} />
-          </Routes>
-        </BrowserRouter>
+      <Navbar bgColor={bgColor} TextColor={TextColor} DarkText={DarkText} ChangeMode={ChangeMode} />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path={"/"} element={<News dark={dark} light={light} TextColor={TextColor} key="general" category="general" />} />
+        </Routes>
+        <Routes>
+          <Route exact path={"/sports"} element={<News dark={dark} light={light} TextColor={TextColor} key="sports" category="sports" />} />
+        </Routes>
+        <Routes>
+          <Route exact path={"/science"} element={<News dark={dark} light={light} TextColor={TextColor} key="science" category="science" />} />
+        </Routes>
+        <Routes>
+          <Route exact path={"/technology"} element={<News dark={dark} light={light} TextColor={TextColor} key="technology" category="technology" />} />
+        </Routes>
+        <Routes>
+          <Route exact path={"/business"} element={<News dark={dark} light={light} TextColor={TextColor} key="technology" category="business" />} />
+        </Routes>
+        <Routes>
+          <Route exact path={"/business"} element={<News dark={dark} light={light} TextColor={TextColor} key="technology" category="business" />} />
+        </Routes>
+        <Routes>
+          <Route exact path={"/entertainment"} element={<News dark={dark} light={light} TextColor={TextColor} key="technology" category="entertainment" />} />
+        </Routes>
+
+      </BrowserRouter>
     </>
   );
 }
